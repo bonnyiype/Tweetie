45907af2dadd1e40913084a8d7e0c1649abbff23
BONNY IYPE <105000155+bonnyiype@users.noreply.github.com>
2023-04-21 11:59:43 +0100

    Update database.js
    refactor

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const connectionString = "mongodb+srv://admin:{YourPassword}@twittercluster0.yfx9fnf.mongodb.net/?retryWrites=true&w=majority"
 
 // Set the 'strictQuery' option to false to disable schema checking for queries
 mongoose.set('strictQuery', false);
@@ -7,18 +8,17 @@ class Database {
     constructor() {
         this.connect();
     }
-
-    connect() {
+    async connect() {
         // Connect to the MongoDB database using the connection string
-        mongoose.connect("mongodb+srv://admin:admin123@twittercluster0.yfx9fnf.mongodb.net/?retryWrites=true&w=majority")
-            .then(() => {
-                console.log("Database connection successful");
-            })
-            .catch((err) => {
-                console.log("Database connection error: " + err);
-            });
-    }
-}
+
+      try { 
+        await mongoose.connect(connectionString);
+          console.log("Database connection successful");
+      } catch (error) {
+            console.error("Database connection error:", error);
+        }
+    }}
+
 
 // Export a new instance of the Database class
 module.exports = new Database();
